7bc82cbdab88f1929ec5ec7c69192ab9ee54a729
KeenanHusselmann <keenan.husselmann39@gmail.com>
2026-03-13 14:14:32 +0200

    Add image quality 70 to next.config

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    formats: ["image/avif", "image/webp"],
+    qualities: [70, 75, 85, 90],
     minimumCacheTTL: 31536000,
     deviceSizes: [640, 750, 828, 1080, 1200, 1920],
     imageSizes: [64, 128, 256, 384, 512],
